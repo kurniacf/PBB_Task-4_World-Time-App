fde00788a6ef54034643690bbba356f912ba81b8
Kurnia Cahya Febryanto <febryanto.2050251@mhs.its.ac.id>
2024-03-31 09:41:54 +0700

    feat: task 31 formatting and show dates

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,11 +1,12 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class WorldTime {
-  late String location; // location name for UI
-  late String time; // the time in that location
-  late String flag; // url to an asset flag icon
-  late String url; // location url for api endpoint
+  String location;
+  late String time;
+  String flag; 
+  String url; 
 
   WorldTime({required this.location, required this.flag, required this.url});
 
@@ -17,14 +18,14 @@ class WorldTime {
 
       String datetime = data['datetime'];
       String offset = data['utc_offset'].substring(1, 3);
-
+      String offsetHours = data['utc_offset'].substring(0,1) == '+' ? offset : '-$offset';
       DateTime now = DateTime.parse(datetime);
-      now = now.add(Duration(hours: int.parse(offset)));
+      now = now.add(Duration(hours: int.parse(offsetHours)));
 
-      time = now.toString();
+      time = DateFormat.jm().format(now);
     } catch (e) {
       print('Caught error: $e');
-      time = 'could not get time';
+      time = 'could not get time data';
     }
   }
 }
