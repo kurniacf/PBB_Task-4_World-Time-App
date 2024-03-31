68d8e644f0943a48f391673e09336bb0e25f38a0
Kurnia Cahya Febryanto <febryanto.2050251@mhs.its.ac.id>
2024-03-31 11:02:26 +0700

    feat: task33 loading and ternary operators

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart'; 
 
 class Home extends StatefulWidget {
   @override
@@ -10,49 +11,87 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    data = data.isNotEmpty ? data : ModalRoute.of(context)?.settings.arguments as Map<dynamic, dynamic>? ?? {};
+    data = data.isNotEmpty ? data : ModalRoute.of(context)?.settings.arguments as Map;
+
+    String bgImage = data['bgImage'];
+    Color bgColor = data['isDaytime'] ? Colors.blue : (Colors.indigo[700] ?? Colors.indigo);
+
+    // Determine the text color based on the background image
+    Color textColor;
+    switch (bgImage) {
+      case 'morning.jpg':
+        textColor = Colors.black87; 
+        break;
+      case 'sunrise.jpg':
+        textColor = Colors.black87; 
+        break;
+      case 'sunset.jpg':
+        textColor = Colors.white; 
+        break;
+      case 'night.jpg':
+        textColor = Colors.white; 
+        break;
+      default:
+        textColor = Colors.black87; 
+    }
 
     return Scaffold(
+      backgroundColor: bgColor,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
-          child: Column(
-            children: <Widget>[
-              TextButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/location').then((result) {
-                    if (result != null) {
-                      setState(() {
-                        data = result as Map<dynamic, dynamic>;
-                      });
-                    }
-                  });
-                },
-                icon: Icon(Icons.edit_location),
-                label: Text('Edit Location'),
-                style: TextButton.styleFrom(foregroundColor: Colors.black),
-              ),
-              SizedBox(height: 20.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    data['location'] ?? 'Location',
-                    style: TextStyle(
-                      fontSize: 28.0,
-                      letterSpacing: 2.0,
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/$bgImage'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+            child: Column(
+              children: <Widget>[
+                TextButton.icon(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/location');
+                  },
+                  icon: Icon(
+                    Icons.edit_location,
+                    color: textColor, 
+                  ),
+                  label: Text(
+                    'Edit Location',
+                    style: GoogleFonts.poppins(
+                      color: textColor, 
+                      fontSize: 16.0, 
                     ),
                   ),
-                ],
-              ),
-              SizedBox(height: 20.0),
-              Text(
-                data['time'] ?? 'Time',
-                style: TextStyle(
-                  fontSize: 66.0,
+                  style: TextButton.styleFrom(
+                    foregroundColor: textColor,
+                  ),
                 ),
-              ),
-            ],
+                SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      data['location'],
+                      style: GoogleFonts.poppins(
+                        color: textColor, 
+                        fontSize: 28.0,
+                        letterSpacing: 2.0,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20.0),
+                Text(
+                  data['time'],
+                  style: GoogleFonts.poppins(
+                    color: textColor, 
+                    fontSize: 66.0,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
